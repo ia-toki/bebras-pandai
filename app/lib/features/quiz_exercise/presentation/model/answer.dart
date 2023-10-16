417892a620266e8b000d904ba80ac6a27c7acdfb
muttaqinrizal <rizalm57@gmail.com>
2023-10-16 20:49:58 +0700

    trailing coma

--- app/lib/features/quiz_exercise/presentation/model/answer.dart
+++ app/lib/features/quiz_exercise/presentation/model/answer.dart
@@ -14,10 +14,11 @@ class Answer {
 
   factory Answer.fromJson(Map<String, dynamic> json) {
     return Answer(
-        aspect: Aspect.fromJson(json['aspect'] as Map<String, dynamic>),
-        correctAnswer:
-            json['correctAnswer'].cast<Map<String, dynamic>>() as List<dynamic>,
-        explanation:
-            Explanation.fromJson(json['explanation'] as Map<String, dynamic>));
+      aspect: Aspect.fromJson(json['aspect'] as Map<String, dynamic>),
+      correctAnswer:
+          json['correctAnswer'].cast<Map<String, dynamic>>() as List<dynamic>,
+      explanation:
+          Explanation.fromJson(json['explanation'] as Map<String, dynamic>),
+    );
   }
 }
