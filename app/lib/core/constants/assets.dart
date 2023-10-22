a64a8bf6369d9884ada10d40ba64cd127e8de4e6
Rosyid <arrosyidbh@gmail.com>
2023-10-22 15:20:56 +0700

    Update image remove background

--- app/lib/core/constants/assets.dart
+++ app/lib/core/constants/assets.dart
@@ -1,5 +1,5 @@
 class Assets {
-  static const logo = 'assets/images/logo.jpg';
+  static const logo = 'assets/images/logo.png';
   static const bebrasPandaiText = 'assets/images/bebras-banner.png';
   static const studyBackground = 'assets/images/study-background.jpg';
   static const bebrasMascot = 'assets/images/bebras-mascot.png';
