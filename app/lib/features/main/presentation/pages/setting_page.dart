a9b8ef19c4c2bcebfb144b8b5d70195bb799882f
Mirza Widihananta <atnanahidiw@gmail.com>
2023-10-24 19:26:31 +0700

    feat: add setting page

--- app/lib/features/main/presentation/pages/setting_page.dart
+++ app/lib/features/main/presentation/pages/setting_page.dart
@@ -1,26 +1,14 @@
 part of '_pages.dart';
 
-class MainPage extends StatefulWidget {
-  const MainPage({super.key});
+class SettingPage extends StatefulWidget {
+  const SettingPage({super.key});
 
   @override
-  State<MainPage> createState() => _MainPageState();
+  State<SettingPage> createState() => _SettingPageState();
 }
 
-class Course {
-  String title;
-  String description;
-  Course(this.title, this.description);
-}
-
-class _MainPageState extends State<MainPage> {
+class _SettingPageState extends State<SettingPage> {
   final nama = 'dummy';
-  final List<Course> courses = [
-    Course('SiKecil', 'abdcdafadf'),
-    Course('Siaga', 'abdcdafadf'),
-    Course('Penggalang', 'abdcdafadf'),
-    Course('Penegak', 'abdcdafadf'),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -39,73 +27,77 @@ class _MainPageState extends State<MainPage> {
                     Assets.bebrasPandaiText,
                   ),
                   const SizedBox(
-                    height: 40,
+                    height: 90,
                   ),
-                  RichText(
-                    text: TextSpan(
-                      text: 'Selamat Datang\n',
-                      style: FontTheme.blackTitle(),
-                      children: <TextSpan>[
-                        TextSpan(
-                          text:
-                              // ignore: lines_longer_than_80_chars
-                              '${FirebaseService.auth().currentUser?.displayName}!',
-                          style: FontTheme.blackTitleBold(),
-                          // recognizer: TapGestureRecognizer()
-                          //   ..onTap = () => context.go('/signup'),
-                        ),
-                      ],
-                    ),
+                  Button(
+                    buttonType: ButtonType.primary,
+                    onTap: () async {
+                      await context.push('/construction');
+                    },
+                    text: 'Edit Profil',
                   ),
                   const SizedBox(
-                    height: 30,
+                    height: 20,
                   ),
                   Button(
                     onTap: () async {
                       final url = Uri.parse(
-                        'https://bebras.or.id/v3/bebras-indonesia-challenge-2023/',
+                        'https://docs.google.com/forms/d/1mUpZXTLvNU93C_bD-HeUTSTBVK1tRDE69t117dkE7ks/edit',
                       );
                       if (!await launchUrl(url)) {
                         throw Exception('Could not launch $url');
                       }
                     },
-                    customButtonColor: Colors.blue,
-                    customTextColor: Colors.white,
-                    text: 'ℹ️  Tentang Tantangan Bebras  ℹ️',
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Button(
-                    buttonType: ButtonType.primary,
-                    onTap: () async {
-                      await context.push('/material');
-                    },
-                    text: 'Lihat / Cetak Materi',
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Button(
-                    buttonType: ButtonType.primary,
-                    onTap: () async {
-                      await context.push('/quiz_registration');
-                    },
-                    text: 'Ikut Latihan Mingguan',
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Button(
-                    onTap: () async {
-                      await context.push('/setting');
-                    },
                     customButtonColor: Colors.grey,
                     customTextColor: Colors.white,
-                    text: 'Pengaturan',
+                    text: 'Hapus Akun',
                   ),
                   const SizedBox(
-                    height: 255,
+                    height: 40,
+                  ),
+                  Button(
+                    buttonType: ButtonType.primary,
+                    onTap: () async {
+                      final url = Uri.parse(
+                        'https://docs.google.com/document/d/1aj1cWrPak3f9On8Bj3KJO68p4MDtZPpWZCBquJZ9Nx0/edit',
+                      );
+                      if (!await launchUrl(url)) {
+                        throw Exception('Could not launch $url');
+                      }
+                    },
+                    text: 'Kebijakan Privasi',
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Button(
+                    onTap: () async {
+                      final url = Uri.parse(
+                        'https://tip.fintix.id/iatoki',
+                      );
+                      if (!await launchUrl(url)) {
+                        throw Exception('Could not launch $url');
+                      }
+                    },
+                    customButtonColor: Colors.amber,
+                    customTextColor: Colors.white,
+                    text: 'Bantu Donasi 💌',
+                  ),
+                  const SizedBox(
+                    height: 200,
+                  ),
+                  Button(
+                    onTap: () async {
+                      await FirebaseAuth.instance.signOut();
+                      await GoogleSignIn().signOut();
+                      context.go('/onboarding');
+                    },
+                    customButtonColor: Colors.red.shade900,
+                    customTextColor: Colors.white,
+                    text: 'Keluar',
+                  ),
+                  const SizedBox(
+                    height: 40,
                   ),
                   InkWell(
                     onTap: () async {
--- app/lib/services/router_service.dart
+++ app/lib/services/router_service.dart
@@ -28,6 +28,10 @@ GoRouter router = GoRouter(
       builder: (context, state) => const MainPage(),
     ),
     GoRoute(
+      path: '/setting',
+      builder: (context, state) => const SettingPage(),
+    ),
+    GoRoute(
       path: '/construction',
       builder: (context, state) => const UnderConstructionPage(),
     ),
