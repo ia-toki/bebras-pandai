1955828222205e38c8dcb4e80c5de4c448fcc351
Mirza Widihananta <atnanahidiw@gmail.com>
2023-11-02 07:55:22 +0700

    fix: buggy update bebras task script

--- utils/scripts/update_bebras_task_collection.py
+++ utils/scripts/update_bebras_task_collection.py
@@ -11,13 +11,13 @@ initialize_app(creds)
 firestore_client = firestore.client()
 col_ref = firestore_client.collection("task_set")
 
-file_list = glob("data/bebras_tasks/*/*/*.json")
+file_list = glob("data/bebras_task/*/*/*.json")
 for file in file_list:
     with open(file, "r") as f:
         file_ref = file.replace(".json", "").split("/")
-        name = "_".join(file_ref[-2:])
 
         content = json.load(f)
-        content["challenge_group"] = file_ref[-2]
+        content["challenge_group"] = file_ref[-3]
 
+        name = "{}_{}".format(file_ref[-3], file_ref[-1])
         col_ref.document(name).set(content)
