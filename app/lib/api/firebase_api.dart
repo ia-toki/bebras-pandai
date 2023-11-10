adc75ea44980bf1317fb5d3d32badb62a9590aed
12henbx <henoch_manampiring@yahoo.com>
2023-11-10 14:32:56 +0700

    add functionality to go to a page after user click notif

--- app/lib/api/firebase_api.dart
+++ app/lib/api/firebase_api.dart
@@ -1,11 +1,36 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 
+import '../services/firebase_service.dart';
+import '../services/router_service.dart';
+
+
 class FirebaseApi {
   final _firebaseMessaging = FirebaseMessaging.instance;
+
+  void handleMessage(RemoteMessage? message) {
+    if (message == null) return;
+
+    router.push('/material');
+  }
+
+  Future initPushNotfications() async {
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
+    FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+
+  }
 
   Future<void> initNotifications() async {
     await _firebaseMessaging.requestPermission();
     final fCMToken = await _firebaseMessaging.getToken();
     print('Token: $fCMToken');
+    initPushNotfications();
   }
 }
