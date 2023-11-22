fc7fe85382e73305f16c76023fbdcf1d32e2b7f8
12henbx <henoch_manampiring@yahoo.com>
2023-11-22 21:47:43 +0700

    change route when handle message

--- app/lib/api/firebase_api.dart
+++ app/lib/api/firebase_api.dart
@@ -11,7 +11,7 @@ class FirebaseApi {
   void handleMessage(RemoteMessage? message) {
     if (message == null) return;
 
-    router.push('/material');
+    router.push('/main');
   }
 
   Future<dynamic> initPushNotfications() async {
