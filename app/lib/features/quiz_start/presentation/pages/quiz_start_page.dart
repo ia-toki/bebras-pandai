cf9e6b2e7ac2fb8b53adb4d5b9053b38773125f1
dandiindra29 <dandiindra29@gmail.com>
2023-12-01 13:45:35 +0700

    add shadow

--- app/lib/features/quiz_start/presentation/pages/quiz_start_page.dart
+++ app/lib/features/quiz_start/presentation/pages/quiz_start_page.dart
@@ -60,8 +60,18 @@ class _QuizStartPageState extends State<QuizStartPage> {
                     children: [
                       DecoratedBox(
                         decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(24)),
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(24),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              spreadRadius: 0,
+                              blurRadius: 4,
+                              offset: const Offset(
+                                  0, 2), // changes position of shadow
+                            ),
+                          ],
+                        ),
                         child: Column(
                           children: [
                             const SizedBox(
