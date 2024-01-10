7b51c2d99eabd320e053c61f97b7c2e63f6f3776
fatkhul jabal <mifjak1@gmail.com>
2024-01-10 13:52:59 +0700

    update url image

--- app/lib/core/constants/assets.dart
+++ app/lib/core/constants/assets.dart
@@ -1,4 +1,6 @@
 class Assets {
+  static const sourceImg = 'sourceImg';
+  static const urlImg = 'https://raw.githubusercontent.com';
   static const flagDir = 'assets/images/flags/';
   static const icon = 'assets/icon/icon.png';
   static const logo = 'assets/images/logo.png';
