c2d7ed53da4beb2d77c53f4432099801bd79b0f2
fatkhul jabal <mifjak1@gmail.com>
2024-01-12 13:11:25 +0700

    update urlImage

--- app/lib/core/constants/assets.dart
+++ app/lib/core/constants/assets.dart
@@ -1,6 +1,6 @@
 class Assets {
-  static const sourceImg = 'sourceImg';
-  static const urlImg = 'https://raw.githubusercontent.com';
+  static const sourceImg = '{sourceImg}';
+  static const urlImg = 'https://storage.googleapis.com/';
   static const flagDir = 'assets/images/flags/';
   static const icon = 'assets/icon/icon.png';
   static const logo = 'assets/images/logo.png';
