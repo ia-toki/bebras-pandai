5f685db955ece3a4bb4bc667870dba9d294cc1f5
fatkhul jabal <mifjak1@gmail.com>
2024-01-24 00:06:51 +0700

    add popup back latihan

--- app/lib/features/quiz_exercise/presentation/pages/task_view.dart
+++ app/lib/features/quiz_exercise/presentation/pages/task_view.dart
@@ -1,12 +1,14 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/bases/enum/button_type.dart';
 import '../../../../core/bases/widgets/atoms/button.dart';
 import '../../../../core/bases/widgets/atoms/html_cached_image.dart';
 import '../../../../core/constants/assets.dart';
 import '../../../../core/theme/font_theme.dart';
+import '../bloc/quiz_exercise_cubit.dart';
 import '../model/quiz_exercise.dart';
 
 class TaskView extends StatelessWidget {
@@ -22,84 +24,126 @@ class TaskView extends StatelessWidget {
     this.remainingDuration,
   });
 
+  Future<bool> _showExitConfirmationDialog(BuildContext context) async {
+    return await showDialog<bool>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Anda akan keluar dari latihan?'),
+        content: const Text('Latihan akan dianggap selesai dengan hasil seadanya'),
+        actions: <Widget>[
+          SizedBox(
+            width: 100,
+            height: 50,
+            child: Button(
+              buttonType: ButtonType.secondary,
+              text: 'Cancel',
+              onTap: () {
+                Navigator.of(context).pop(false);
+              },
+            ),
+          ),
+          SizedBox(
+            width: 100,
+            height: 50,
+            child: Button(
+              buttonType: ButtonType.tertiary,
+              text: 'Ya',
+              onTap: () {
+                context.read<QuizExerciseCubit>().finishExerciseTimeUp();
+                Navigator.of(context).pop(true);
+              }
+            )
+          )
+        ],
+      ),
+    ) ?? false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        if (remainingDuration != null)
-          Container(
-            color: Colors.yellowAccent,
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            child: Text(
-                '${remainingDuration!.inMinutes.toString().padLeft(2, '0')} : ${remainingDuration!.inSeconds.remainder(60).toString().padLeft(2, '0')}'),
-          ),
-        if (remainingDuration != null)
-          const SizedBox(
-            height: 10,
-          ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Image.asset(
-              '${Assets.flagDir}${task.country}.png',
-              width: 40,
-              height: 20,
-            ),
-            Flexible(
+    return WillPopScope(
+      onWillPop: () async {
+        // Display the popup when the back button is pressed
+        bool result = await _showExitConfirmationDialog(context);
+        return result;
+      },
+      child: Column(
+        children: [
+          if (remainingDuration != null)
+            Container(
+              color: Colors.yellowAccent,
+              padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
               child: Text(
-                task.title,
-                textAlign: TextAlign.center,
-                style: FontTheme.blackSubtitleBold(),
+                  '${remainingDuration!.inMinutes.toString().padLeft(2, '0')} : ${remainingDuration!.inSeconds.remainder(60).toString().padLeft(2, '0')}'),
+            ),
+          if (remainingDuration != null)
+            const SizedBox(
+              height: 10,
+            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                '${Assets.flagDir}${task.country}.png',
+                width: 40,
+                height: 20,
+              ),
+              Flexible(
+                child: Text(
+                  task.title,
+                  textAlign: TextAlign.center,
+                  style: FontTheme.blackSubtitleBold(),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    task.id,
+                    style: const TextStyle(fontSize: 9),
+                  ),
+                  Text(
+                    task.source,
+                    style: const TextStyle(fontSize: 7),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Container(
+            height: 2,
+            color: Colors.black,
+          ),
+          const SizedBox(
+            height: 3,
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height - 340,
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            decoration: BoxDecoration(border: Border.all()),
+            child: SingleChildScrollView(
+              child: HtmlWithCachedImages(
+                // ignore: prefer_adjacent_string_concatenation
+                // data: '<h3>Deskripsi</h3>${task.description.content}' +
+                //     '<h3>Pertanyaan</h3>${task.question.content}',
+                data: task.description.content.replaceAll(Assets.sourceImg, Assets.urlImg),
               ),
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  task.id,
-                  style: const TextStyle(fontSize: 9),
-                ),
-                Text(
-                  task.source,
-                  style: const TextStyle(fontSize: 7),
-                ),
-              ],
-            ),
-          ],
-        ),
-        Container(
-          height: 2,
-          color: Colors.black,
-        ),
-        const SizedBox(
-          height: 3,
-        ),
-        Container(
-          height: MediaQuery.of(context).size.height - 340,
-          width: double.infinity,
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          decoration: BoxDecoration(border: Border.all()),
-          child: SingleChildScrollView(
-            child: HtmlWithCachedImages(
-              // ignore: prefer_adjacent_string_concatenation
-              // data: '<h3>Deskripsi</h3>${task.description.content}' +
-              //     '<h3>Pertanyaan</h3>${task.question.content}',
-              data: task.description.content.replaceAll(Assets.sourceImg, Assets.urlImg),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          SizedBox(
+            width: 150,
+            child: Button(
+              text: 'JAWAB',
+              buttonType: ButtonType.primary,
+              onTap: onTap,
             ),
           ),
-        ),
-        const SizedBox(
-          height: 14,
-        ),
-        SizedBox(
-          width: 150,
-          child: Button(
-            text: 'JAWAB',
-            buttonType: ButtonType.primary,
-            onTap: onTap,
-          ),
-        )
-      ],
+        ],
+      )
     );
   }
 }
