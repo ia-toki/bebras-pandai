1cc6ea8b8a6aa4d67a3817a13c8402f1447fa441
fatkhul jabal <mifjak1@gmail.com>
2024-01-25 21:35:42 +0700

    update choice html text

--- app/lib/features/quiz_exercise/presentation/pages/task_dialog.dart
+++ app/lib/features/quiz_exercise/presentation/pages/task_dialog.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/bases/enum/button_type.dart';
 import '../../../../core/bases/widgets/atoms/button.dart';
-// import '../../../../core/bases/widgets/atoms/html_cached_image.dart';
+import '../../../../core/bases/widgets/atoms/html_cached_image.dart';
 import '../../../../core/constants/assets.dart';
 // import '../../../../core/theme/font_theme.dart';
 import '../../../authentication/register/presentation/widgets/custom_text_field.dart';
@@ -69,7 +69,9 @@ class TaskDialog extends StatelessWidget {
                   width: MediaQuery.of(context).size.width - 240,
                                   )
                                 : Flexible(
-                                    child: Text(e.value.content),
+                                    child: HtmlWithCachedImages(
+                                        data:e.value.content
+                                    ),
                                   )
                           ],
                         ),
