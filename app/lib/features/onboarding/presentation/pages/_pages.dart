a870eeb79f7625f6a731fc3ab018c90cd2b1d042
fatkhul jabal <mifjak1@gmail.com>
2024-02-26 14:03:38 +0700

    update copywriting versi terbaru & menmabahkan button close apps

--- app/lib/features/onboarding/presentation/pages/_pages.dart
+++ app/lib/features/onboarding/presentation/pages/_pages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:url_launcher/url_launcher.dart';
